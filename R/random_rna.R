db1b64fa1e6b55a4deb09d4b12c1a825b31c14f8
JulieMariaJ <s174595@student.dtu.dk>
2021-03-22 10:36:38 +0000

    random rna with documentation

--- R/random_rna.R
+++ R/random_rna.R
@@ -1,6 +1,15 @@
 # R/random_rna.R
+#' Function that returns a random RNA string
+#'
+#' @param l 
+#'
+#' @return A string of RNA with a random sequence of a given length 
+#' @export
+#'
+#' @examples
+#' random_rna(10)
 random_rna <- function(l){
   nucleotides <- sample(c("A", "U", "G", "C"), size = l, replace = TRUE)
-  dna = paste0(nucleotides, collapse = "")
-  return(dna)
+  rna = paste0(nucleotides, collapse = "")
+  return(rna)
 }
