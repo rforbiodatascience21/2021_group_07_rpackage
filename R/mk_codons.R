88a42aca9471dd58b2aedd20edc777cf2d7d3d68
simone-majken <s174596@student.dtu.dk>
2021-03-22 10:36:15 +0000

    Added description to mk_codons function

--- R/mk_codons.R
+++ R/mk_codons.R
@@ -1,4 +1,18 @@
 # R/mk_codons.R
+#' Create Codons
+#' 
+#' This function creates codons in a DNA string
+#' 
+#' @param l length of desired DNA string
+#' 
+#' @return 
+#' DNA string
+#' 
+#' @examples 
+#' mk_codons(dna, s=1)
+#' 
+#' @export
+
 mk_codons <- function(dna, s = 1){
   l = nchar(dna)
   codons <- substring(dna,
@@ -6,3 +20,4 @@ mk_codons <- function(dna, s = 1){
                       last = seq(from = 3+s-1, to = l, by = 3))
   return(codons)
 }
+
